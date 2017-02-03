cf2430a687ee558e0580b28237da8e90b626babf
Christian Paling <christian.paling@googlemail.com>
2017-02-03 10:41:06 +0100

    Add basic worker

--- app/services/ReminderMailWorkerPool.scala
+++ app/services/ReminderMailWorkerPool.scala
@@ -3,9 +3,15 @@ package services
 import javax.inject._
 import play.api.inject.ApplicationLifecycle
 import com.rabbitmq.client._
+import play.Configuration
+import play.api.Logger
+import play.api.libs.mailer._
+
+import persistency._
+import mailers._
 
 @Singleton
-class ReminderMailWorkerPool @Inject() (lifecycle: ApplicationLifecycle) {
+class ReminderMailWorkerPool @Inject() (lifecycle: ApplicationLifecycle, reminderRepo: ReminderRepository, userRepo: UserRepository, config: Configuration, mailerClient: MailerClient) {
 
   val queueName = "hello"
   val factory = new ConnectionFactory()
@@ -19,7 +25,17 @@ class ReminderMailWorkerPool @Inject() (lifecycle: ApplicationLifecycle) {
 	val consumer = new DefaultConsumer(channel) {
 		override def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]) {
 			val message = new String(body, "UTF-8")
-			println(message)
+      val userId = message.split(",")(0).toInt
+      val reminderId = message.split(",")(1).toInt
+
+      val user = userRepo.findUserById(userId)
+      val reminder = reminderRepo.byUserIdAndReminderId(userId, reminderId)
+
+      val mail = ReminderMailer.constructMail(config.getString("play.mailer.user"), user.get.email, reminder.get.message)
+      mailerClient.send(mail)
+
+      reminderRepo.deleteByReminderId(reminderId)
+      Logger.debug(s"sent user: ${user.get.email}, reminder: ${reminder.get.message}")
 		}
 	}
 
