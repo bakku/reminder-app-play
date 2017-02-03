d3196f46d6acf4c1ea38438e7af1aade99eedb65
Christian Paling <christian.paling@googlemail.com>
2017-02-03 10:38:53 +0100

    Add more text to mailer

--- app/mailers/ReminderMailer.scala
+++ app/mailers/ReminderMailer.scala
@@ -5,11 +5,38 @@ import play.api.libs.mailer._
 
 object ReminderMailer {
   def constructMail(from: String, to: String, message: String): Email = {
+    val textTemplate = """Hi
+                         |
+                         |You wanted to get reminded right now of the following message:
+                         |
+                         |{message}
+                         |
+                         |I hope I was of help
+                         |
+                         |Best regards,
+                         |
+                         |Your Reminder""".stripMargin
+
+    val emailTemplate = """<p>
+                          |Hi!<br><br>
+                          |
+                          |You wanted to get reminded right now of the following message:<br><br>
+                          |
+                          |{message}<br><br>
+                          |
+                          |I hope I was of help<br><br>
+                          |
+                          |Best regards,<br><br>
+                          |
+                          |Your Reminder
+                          |</p>""".stripMargin
+
     Email(
       "Your reminder",
       "Reminder App <" + from + ">",
       Seq(to + "<" + to + ">"),
-      bodyText = Some(message)
+      bodyText = Some(textTemplate.replace("{message}", message)),
+      bodyHtml = Some(emailTemplate.replace("{message}", message))
     )
   }
 }
